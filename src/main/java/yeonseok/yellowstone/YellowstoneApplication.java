3b790c4c264888bf3184f081a4cc5857da929667
yeonnseok <ljinsk3@gmail.com>
2020-12-23 23:30:51 +0900

    first commit

---
+++ src/main/java/yeonseok/yellowstone/YellowstoneApplication.java
@@ -0,0 +1,13 @@
+package yeonseok.yellowstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeonseok/yellowstone/YellowstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package yeonseok.yellowstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
